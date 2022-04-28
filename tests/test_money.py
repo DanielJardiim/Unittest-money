328c7a806e589d5ea7bd1c586b82fe8a9cc385dd
Daniel Jardim <jn_dani@outlook.com>
2022-04-27 22:42:47 -0300

    Atualizando test_money.py

--- tests/test_money.py
+++ tests/test_money.py
@@ -1,17 +1,32 @@
 import unittest
 
-from src.pcr_money.money import Money
+from src.money.money import Money
 
 class TestMoney(unittest.TestCase):
-    def test_handle_value_round(self):
-        self.assertEqual(Money.handle_value(10.774,2,"round"),10.77)
-        self.assertEqual(Money.handle_value(10.777,2,"round"),10.78)
-        self.assertEqual(Money.handle_value(10.77,2,"round"),10.77)
+    def test_value_round_baixo(self):
+        valor1 = Money.value(10.774,2,"round")
+        self.assertEqual(valor1,10.77)
+        
+    def test_value_round_alto(self):
+        valor2 = Money.value(10.778,2,"round")
+        self.assertEqual(valor2,10.78)
 
-    def test_handle_value_truncation(self):
-        self.assertEqual(Money.handle_value(10.774,2,"truncation"),10.77)
-        self.assertEqual(Money.handle_value(10.777,2,"truncation"),10.77)
-        self.assertEqual(Money.handle_value(10.771,2,"truncation"),10.77)
+    def test_value_round_mantem(self):
+        valor3 = Money.value(10.77,2,"round")
+        self.assertEqual(valor3,10.77)
 
-if __name__ == "__name__":
-    unittest.main()
+    def test_value_truncation_baixo(self):
+        valor4 = Money.value(10.774,2,"truncation")
+        self.assertEqual(valor4,10.77)
+
+    def test_value_truncation_alto(self):
+        valor5 = Money.value(10.778,2,"truncation")
+        self.assertEqual(valor5,10.77)
+
+    def test_value_truncation_mantem(self):
+        valor6 = Money.value(10.77,2,"truncation")
+        self.assertEqual(valor6,10.77)
+
+    def test_value_error_method(self):
+        valor7 = Money.value(10.568,2,"error")
+        self.assertFalse(valor7)
