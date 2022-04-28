21c708eced9bf8c5b9bc1ec06938c237c05e2b2d
Daniel Jardim <jn_dani@outlook.com>
2022-04-27 22:45:16 -0300

    Atualizando src/money/

---
+++ src/money/money.py
@@ -0,0 +1,15 @@
+from requests import get  #biblioteca que permite enviar solicitações HTTP em python. (API)
+import math
+
+class Money():
+
+    def value(value, decimals, method):  #Arredondamento 10.787 -> 10.79 / 10.784 -> 10.78
+        if(method == 'round'):
+            return round(value, decimals)
+        elif(method == 'truncation'):  #Truncamento 10.787 -> 10.78
+            multiplier = pow(10, decimals)
+            value_int = int(value * multiplier)
+            return float(value_int/multiplier)
+        else:
+            print("Method não encontrado!")
+            return False
